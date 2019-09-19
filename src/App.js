e075e5e8ec37ccccedf68edd74d726b2bb0f27ed
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-19 21:38:06 +0200

    class app component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Title from "./components/Title/Title";
 import Map from "./components/Map/Map";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <Title />
-      <Map />
-    </>
-  );
+class App extends React.Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <Title />
+        <Map />
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -3,6 +3,16 @@ import SVG from "react-inlinesvg";
 import map from "../../images/worldHigh3.svg";
 import "./Map.module.scss";
 
+// class Map extends React.Component {
+//   state = {};
+//   handleClick = e => {
+//     console.log(e.target);
+//   };
+//   render() {
+//     return <SVG src={map} onClick={this.handleClick} />;
+//   }
+// }
+
 const Map = () => {
   return <SVG src={map} />;
 };
