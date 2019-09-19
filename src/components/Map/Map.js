9e549dd92d0d50f5db0a5b4e1abe716c8cfe25ca
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-19 17:31:09 +0200

    styling svg by classic css

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SVG from "react-inlinesvg";
 import map from "../../images/worldHigh3.svg";
+import "./Map.css";
 
 const Map = () => {
   return <SVG src={map} />;
