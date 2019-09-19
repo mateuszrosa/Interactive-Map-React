c41f2866c0496e46eda9d912344c45bd3624afeb
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-19 17:18:15 +0200

    added react-inlinesvg and svg world map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Title from "./components/Title/Title";
+import Map from "./components/Map/Map";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Title />
+      <Map />
     </>
   );
 }
---
+++ src/components/Map/Map.js
@@ -0,0 +1,9 @@
+import React from "react";
+import SVG from "react-inlinesvg";
+import map from "../../images/worldHigh3.svg";
+
+const Map = () => {
+  return <SVG src={map} />;
+};
+
+export default Map;
