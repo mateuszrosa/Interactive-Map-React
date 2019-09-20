55cfa680501e8a211a06f544cf9e088dcfbdaf76
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-20 19:25:54 +0200

    added input component

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Title from "../Title/Title";
+import Input from "./Input/Input";
 import "./Section.module.scss";
 
 const Section = () => {
   return (
     <section>
       <Title type="section" text="Find information about that country" />
+      <Input />
     </section>
   );
 };
