8b28a2056404b01a94ea2ed6886ac6605b224840
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-23 15:35:33 +0200

    adding dates from fetch to h1

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends React.Component {
     e.target.style.fill = "gray";
     let value = e.target.getAttribute("title");
     this.handleTest(value);
+    this.handleFetch(value);
   };
   handleTest = text => {
     const h1 = document.querySelector(".info h1");
@@ -25,6 +26,24 @@ class App extends React.Component {
   handleInput = e => {
     const h1 = document.querySelectorAll(".info h1");
     h1[0].textContent = "";
+  };
+  handleFetch = value => {
+    const h1 = document.querySelectorAll(".info h1");
+    fetch(`https://restcountries.eu/rest/v2/name/` + value)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data => {
+        let country = data[0];
+        h1[0].innerHTML = `Name: <span class="inf">${country.name}</span>`;
+        h1[1].innerHTML = `Region: <span class="inf">${country.region}</span>`;
+        h1[2].innerHTML = `Subegion: <span class="inf">${country.subregion}</span>`;
+        h1[3].innerHTML = `Native name: <span class="inf">${country.nativeName}</span>`;
+        h1[4].innerHTML = `Capital: <span class="inf">${country.capital}</span>`;
+        h1[5].innerHTML = `Language: <span class="inf">${country.languages[0].name}</span>`;
+        h1[6].innerHTML = `Currency: <span class="inf">${country.currencies[0].code}</span>`;
+        h1[7].innerHTML = `Population: <span class="inf">${country.population.toLocaleString()}</span>`;
+      }, false);
   };
 
   render() {
