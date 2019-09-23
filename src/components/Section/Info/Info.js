306015e594cf03ac487850c37fd2079ffda7e02e
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-23 23:46:54 +0200

    state info

--- src/components/Section/Info/Info.js
+++ src/components/Section/Info/Info.js
@@ -4,11 +4,28 @@ import Img from "./Img/Img";
 import "./Info.module.scss";
 
 const Info = props => {
-  const { name } = props.info;
-  console.log(name);
+  const {
+    name,
+    region,
+    subregion,
+    nativeName,
+    capital,
+    language,
+    currency,
+    population,
+    img
+  } = props.info;
   return (
     <div className="info">
       <Heading text={`Name: ${name}`} />
+      <Heading text={`Region: ${region}`} />
+      <Heading text={`Subregion: ${subregion}`} />
+      <Heading text={`Native name: ${nativeName}`} />
+      <Heading text={`Capital: ${capital}`} />
+      <Heading text={`Language: ${language}`} />
+      <Heading text={`Currency: ${currency}`} />
+      <Heading text={`Population: ${population}`} />
+      <Img src={img} />
     </div>
   );
 };
