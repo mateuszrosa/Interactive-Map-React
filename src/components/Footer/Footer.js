ba50a9d1fd224e0a822b3e1b5a292c9b6b433c26
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-23 11:43:57 +0200

    Link inside Heading inside Footer components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Heading from "../Heading/Heading";
+import Link from "./Link/Link";
 import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer>
-      <Heading size="h3" text="footer" />
+      <Heading size="h3" text={<Link text="Link1" />} />
     </footer>
   );
 };
---
+++ src/components/Footer/Link/Link.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Link = ({ text, href }) => {
+  return <a href={href}>{text}</a>;
+};
+
+export default Link;
