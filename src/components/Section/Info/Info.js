073d7871553f1b77d4e05ec642e46d55cecb34fd
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-23 13:23:40 +0200

    info component

---
+++ src/components/Section/Info/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "../../Heading/Heading";
+import "./Info.module.scss";
+
+const Info = () => {
+  return (
+    <div className="info">
+      <Heading text="test" />
+    </div>
+  );
+};
+
+export default Info;
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Heading from "../Heading/Heading";
 import Form from "./Form/Form";
+import Info from "./Info/Info";
 import "./Section.module.scss";
 
 const Section = () => {
@@ -8,6 +9,7 @@ const Section = () => {
     <section>
       <Heading type="section" text="Find information about that country" />
       <Form />
+      <Info />
     </section>
   );
 };
