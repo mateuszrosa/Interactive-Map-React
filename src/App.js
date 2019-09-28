c4fdce404ff3b0f4fc3c6f3dde925dc2e1637e9b
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-28 16:16:50 +0200

    box-shadow and margin-top of img

--- src/App.js
+++ src/App.js
@@ -106,6 +106,7 @@ class App extends React.Component {
         return resp.json();
       })
       .then(data => {
+        console.log(data);
         let country = data[0];
         if (value === "India") {
           country = data[1];
