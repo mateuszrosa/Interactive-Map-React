f2bb613f5448c0723eab0f27b6909a9975bd9019
mateuszrosa <mateusz.rosa@gmail.com>
2019-09-30 13:32:57 +0200

    edited conditional rednering

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,22 +1,38 @@
 import React from "react";
 import styles from "components/Heading/Heading.module.scss";
 
-const Heading = ({ children, size = "h1", type, comp }) => {
-  if (size === "h1") {
-    return (
+// const Heading = ({ children, size = "h1", type, comp }) => {
+//   if (size === "h1") {
+//     return (
+//       <h1 className={type === "title" ? styles.title : styles.section}>
+//         {children}
+//         {comp}
+//       </h1>
+//     );
+//   } else {
+//     return (
+//       <h3 className={type === "title" ? styles.title : styles.section}>
+//         {children}
+//         {comp}
+//       </h3>
+//     );
+//   }
+// };
+
+const Heading = ({ children, size = "h1", type, comp }) => (
+  <>
+    {size === "h1" ? (
       <h1 className={type === "title" ? styles.title : styles.section}>
         {children}
         {comp}
       </h1>
-    );
-  } else {
-    return (
+    ) : (
       <h3 className={type === "title" ? styles.title : styles.section}>
         {children}
         {comp}
       </h3>
-    );
-  }
-};
+    )}
+  </>
+);
 
 export default Heading;
