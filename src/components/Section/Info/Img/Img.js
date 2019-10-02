fa528df2bcdcfb4ac79ecfbfa0a51886c85f727e
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-02 16:14:49 +0200

    img component styled

--- src/components/Section/Info/Img/Img.js
+++ src/components/Section/Info/Img/Img.js
@@ -1,8 +1,15 @@
 import React from "react";
-import "components/Section/Info/Img/Img.module.scss";
+import styled from "styled-components";
+
+const StyledImg = styled.img`
+  width: 50%;
+  display: block;
+  margin: 10px auto;
+  box-shadow: 0 0 3px 3px gray;
+`;
 
 const Img = ({ src, alt }) => {
-  return <img src={src} alt={alt} />;
+  return <StyledImg src={src} alt={alt} />;
 };
 
 export default Img;
