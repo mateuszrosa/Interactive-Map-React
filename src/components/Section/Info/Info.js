db9bf474eefd16a5f28b1bf0c01f8b782b6d9e5f
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-04 16:36:44 +0200

    removed appcontext from info component

--- src/components/Section/Info/Info.js
+++ src/components/Section/Info/Info.js
@@ -1,5 +1,4 @@
 import React from "react";
-import AppContext from "context";
 import Paragraph from "components/Paragraph/Paragraph";
 import Img from "components/Section/Info/Img/Img";
 import Span from "components/Section/Info/Span/Span";
@@ -19,21 +18,17 @@ const Info = props => {
     img
   } = props.info;
   return (
-    <AppContext.Consumer>
-      {context => (
-        <div className={display ? styles.show : styles.info}>
-          <Paragraph comp={<Span text={name} />}>Name: </Paragraph>
-          <Paragraph comp={<Span text={region} />}>Region: </Paragraph>
-          <Paragraph comp={<Span text={subregion} />}>Subregion: </Paragraph>
-          <Paragraph comp={<Span text={nativeName} />}>Native name: </Paragraph>
-          <Paragraph comp={<Span text={capital} />}>Capital: </Paragraph>
-          <Paragraph comp={<Span text={language} />}>Language: </Paragraph>
-          <Paragraph comp={<Span text={currency} />}>Currency: </Paragraph>
-          <Paragraph comp={<Span text={population} />}>Population: </Paragraph>
-          <Img src={img} />
-        </div>
-      )}
-    </AppContext.Consumer>
+    <div className={display ? styles.show : styles.info}>
+      <Paragraph comp={<Span text={name} />}>Name: </Paragraph>
+      <Paragraph comp={<Span text={region} />}>Region: </Paragraph>
+      <Paragraph comp={<Span text={subregion} />}>Subregion: </Paragraph>
+      <Paragraph comp={<Span text={nativeName} />}>Native name: </Paragraph>
+      <Paragraph comp={<Span text={capital} />}>Capital: </Paragraph>
+      <Paragraph comp={<Span text={language} />}>Language: </Paragraph>
+      <Paragraph comp={<Span text={currency} />}>Currency: </Paragraph>
+      <Paragraph comp={<Span text={population} />}>Population: </Paragraph>
+      <Img src={img} />
+    </div>
   );
 };
 
