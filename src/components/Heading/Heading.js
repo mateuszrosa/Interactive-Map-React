e05d4561bb192d1e25fdd03c2dd15f24bc734c1d
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-05 16:05:44 +0200

    media queries for headings

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -4,7 +4,10 @@ import styled from "styled-components";
 let StyledHeading = styled.h1`
   margin: 0 auto 0 auto;
   text-align: center;
-  font-size: ${props => (props.type === "title" ? "60px" : "24px")};
+  font-size: ${props => (props.type === "title" ? "30px" : "14px")};
+  @media (min-width: 375px) {
+    font-size: ${props => (props.type === "title" ? "60px" : "24px")};
+  }
 `;
 
 const Heading = ({ children, type }) => {
