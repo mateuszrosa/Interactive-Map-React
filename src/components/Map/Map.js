f2ca09994b649262a67f712c35539c9b9c05cca9
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-07 16:00:29 +0200

    fixed css

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,13 +6,9 @@ import styled from "styled-components";
 const StyledSVG = styled(SVG)`
   display: block;
   margin: 80px auto;
-  width: 100%;
+  width: 55%;
   height: 65vh;
-  transform: scale(0.6);
-  @media (min-width: 375px) {
-    transform: scale(1.1);
-    width: 55%;
-  }
+  transform: scale(1.1);
 `;
 
 const Map = props => {
