007d66aba04d7081f2375bfd8e869cb699d5df2f
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-16 16:25:54 +0200

    span proptypes

--- src/components/Span/Span.js
+++ src/components/Span/Span.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledSpan = styled.span`
   font-weight: 400;
 `;
 
 const Span = ({ children }) => <StyledSpan>{children}</StyledSpan>;
+
+Span.propTypes = {
+  children: PropTypes.string.isRequired,
+};
 
 export default Span;
