6c208a47d4a65a1402a0d6a0751d26d8e98b55f0
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-16 12:03:00 +0200

    proptypes for label component

--- src/components/Section/Form/Label/Label.js
+++ src/components/Section/Form/Label/Label.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledLabel = styled.label`
   margin-right: 10px;
 `;
 
 const Label = ({ text, id }) => <StyledLabel htmlFor={id}>{text}</StyledLabel>;
+
+Label.propTypes = {
+  text: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default Label;
