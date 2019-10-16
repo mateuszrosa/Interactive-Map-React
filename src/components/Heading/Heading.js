6264abd33b98f30f53fb3a07eeb61702a0161a0d
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-16 16:24:14 +0200

    heading proptypes

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledHeading = styled.h1`
   margin: 0 auto 0 auto;
   text-align: center;
-  font-size: ${(props) => (props.type === 'title' ? '30px' : '14px')};
+  font-size: ${props => (props.type === 'title' ? '30px' : '14px')};
   @media (min-width: 375px) {
-    font-size: ${(props) => (props.type === 'title' ? '60px' : '24px')};
+    font-size: ${props => (props.type === 'title' ? '60px' : '24px')};
   }
 `;
 
-const Heading = ({ children, type }) => (
-  <StyledHeading type={type}>{children}</StyledHeading>
-);
+const Heading = ({ children, type }) => <StyledHeading type={type}>{children}</StyledHeading>;
+
+Heading.propTypes = {
+  children: PropTypes.string.isRequired,
+  type: PropTypes.string,
+};
+
+Heading.defaultProps = {
+  type: '',
+};
 
 export default Heading;
