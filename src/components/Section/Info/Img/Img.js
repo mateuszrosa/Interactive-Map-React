81aeafd81349d14842fde4be66a36ca07fb49a37
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-21 12:12:32 +0200

    img component

--- src/components/Section/Info/Img/Img.js
+++ src/components/Section/Info/Img/Img.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 const StyledImg = styled.img`
   width: 50%;
@@ -9,16 +7,4 @@ const StyledImg = styled.img`
   box-shadow: 0 0 3px 3px gray;
 `;
 
-const Img = ({ src, alt }) => <StyledImg src={src} alt={alt} />;
-
-Img.propTypes = {
-  src: PropTypes.string,
-  alt: PropTypes.string,
-};
-
-Img.defaultProps = {
-  src: '',
-  alt: '',
-};
-
-export default Img;
+export default StyledImg;
