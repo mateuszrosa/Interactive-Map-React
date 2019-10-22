0528f95bbe15128f23a8484d23d0c8cd8a5b655b
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-22 18:32:13 +0200

    removed media query for heading

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -6,9 +6,6 @@ const StyledHeading = styled.h1`
   margin: 0 auto 0 auto;
   text-align: center;
   font-size: ${props => (props.type === 'title' ? '30px' : '14px')};
-  @media (min-width: 375px) {
-    font-size: ${props => (props.type === 'title' ? '60px' : '24px')};
-  }
 `;
 
 const Heading = ({ children, type }) => <StyledHeading type={type}>{children}</StyledHeading>;
