4c748408a6ed5708d126f3a3afe58109a7d7b7f8
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-22 18:36:06 +0200

    styledheading component

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -1,22 +1,9 @@
-import React from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
 
 const StyledHeading = styled.h1`
   margin: 0 auto 0 auto;
   text-align: center;
-  font-size: ${props => (props.type === 'title' ? '30px' : '14px')};
+  font-size: ${props => (props.type === 'title' ? '60px' : '24px')};
 `;
 
-const Heading = ({ children, type }) => <StyledHeading type={type}>{children}</StyledHeading>;
-
-Heading.propTypes = {
-  children: PropTypes.string.isRequired,
-  type: PropTypes.string,
-};
-
-Heading.defaultProps = {
-  type: '',
-};
-
-export default Heading;
+export default StyledHeading;
