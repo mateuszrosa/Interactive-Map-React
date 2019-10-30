f6e3c72e091155dd1d34d665f0c798ac08b9afda
mateuszrosa <mateusz.rosa@gmail.com>
2019-10-30 15:10:38 +0100

    media queries for img

--- src/components/atoms/Img/Img.js
+++ src/components/atoms/Img/Img.js
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 
 const StyledImg = styled.img`
-  width: 50%;
+  width: 40%;
   display: block;
   margin: 10px auto;
   box-shadow: 0 0 3px 3px gray;
+  @media (min-width: 768px) {
+    width: 20%;
+  }
+  @media (min-width: 1440px) {
+    width: 50%;
+  }
 `;
 
 export default StyledImg;
