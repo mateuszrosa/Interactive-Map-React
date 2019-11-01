39dc7060b20d21a4483ac35bc3ab878fe4ff6fc5
mateuszrosa <mateusz.rosa@gmail.com>
2019-11-01 15:46:52 +0100

    heading media queries

--- src/components/atoms/Heading/Heading.js
+++ src/components/atoms/Heading/Heading.js
@@ -3,10 +3,7 @@ import styled from 'styled-components';
 const StyledHeading = styled.h1`
   margin: 0 auto 0 auto;
   text-align: center;
-  font-size: ${props => (props.type === 'title' ? '40px' : '14px')};
-  @media (min-width: 768px) {
-    font-size: ${props => (props.type === 'title' ? '60px' : '24px')};
-  }
+  font-size: ${props => (props.type === 'title' ? '40px' : '20px')};
   @media (min-width: 1024px) {
     font-size: ${props => (props.type === 'title' ? '50px' : '14px')};
   }
