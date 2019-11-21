e0c0014c1660d0ddafc38073eb1006b72b2caa0f
mateuszrosa <mateusz.rosa@gmail.com>
2019-11-21 21:56:04 +0100

    media queries for inputs

--- src/components/molecules/Form/Form.js
+++ src/components/molecules/Form/Form.js
@@ -6,6 +6,12 @@ import PropTypes from 'prop-types';
 
 const StyledForm = styled.form`
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (min-width: 375px) {
+    display: block;
+  }
 `;
 
 const Form = ({ click, submit, input, id, placeholder }) => {
