fae8406449e1154e42e6ccbb2e26a4e2b222b526
mateuszrosa <mateusz.rosa@gmail.com>
2019-11-22 18:40:08 +0100

    form styling

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   border-radius: 5px;
-  width: ${({ submit }) => (submit ? '70px' : '150px')};
+  width: ${({ submit }) => (submit ? 'auto' : '150px')};
   margin-left: ${({ submit }) => (submit ? '10px' : '0')};
   ::placeholder {
     color: ${props => (props.error ? 'red' : 'black')};
