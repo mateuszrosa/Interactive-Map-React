a364e6f5af31a3f814da5d45cde301a55ef25c6a
mateuszrosa <mateusz.rosa@gmail.com>
2020-02-04 16:09:38 +0100

    edited width of input

--- src/components/atoms/Input/Input.js
+++ src/components/atoms/Input/Input.js
@@ -2,12 +2,17 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   border-radius: 5px;
-  width: ${({ submit }) => (submit ? '60px' : '215px')};
+  width: ${({ submit }) => (submit ? '60px' : '185px')};
   margin-left: ${({ submit }) => (submit ? '5px' : '0')};
   background-color: #fff;
   ::placeholder {
     color: ${({ error }) => (error ? 'red' : 'black')};
   }
+
+  @media (min-width: 768px) {
+    width: ${({ submit }) => (submit ? '60px' : '200px')};
+  }
+
   @media (min-width: 1440px) {
     width: ${({ submit }) => (submit ? '60px' : '160px')};
   }
