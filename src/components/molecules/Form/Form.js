be1e6a452eb0b4b6584e1be86e5fc43abb00fd49
mateuszrosa <mateusz.rosa@gmail.com>
2020-06-19 18:16:45 +0200

    edited react import in Form

--- src/components/molecules/Form/Form.js
+++ src/components/molecules/Form/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import List from 'components/molecules/List/List';
 import Input from 'components/atoms/Input/Input';
